be58e9aecaab189828fa13a78e43dc0abc4e238c
Nawel <nawelamaninawel@gmail.com>
2025-11-02 23:11:06 +0100

    Updated script.js content

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-document.getElementById("btnrefresh").onclick = function(){
+document.getElementById("btnrefresh").onclick = function() {
+  let ligne = document.querySelectorAll("table tr");
 
-  let ligne = document.querySelectorAll(".table tr");
-  
-  for(let i = 2; i < ligne.length; i++){
+  for (let i = 2; i < ligne.length; i++) {  // start at 2 to skip headers
     let abs = 0;
     let par = 0;
-    let check = document.querySelectorAll("input[type=checkbox]");
+    let boxes = ligne[i].querySelectorAll("input[type=checkbox]");
 
-    for(j=0; j < check.length; j += 2){
-      if(!check[j].checked) abs++;
-    }
-    for(k = 1; k < check.length; k+=2){
-      if(check[k].checked) par++;
+    for (let j = 0; j < boxes.length; j += 2) {
+      if (!boxes[j].checked) abs++;
     }
 
-    ligne[i].cells[14].textContent = abs + "Abs";
-    ligne[i].cells[15].textContent = par + "Par";
+    for (let k = 1; k < boxes.length; k += 2){
+      if(boxes[k].checked) par++;
+    }
+   
+    ligne[i].cells[14].textContent = abs + " Abs";
+    ligne[i].cells[15].textContent = par + "Par"
+  
+
+  if(abs < 3){
+   ligne[i].style.backgroundColor = "lightgreen";
+   
+  }else if(abs >= 3 && abs <= 4) {
+   ligne[i].style.backgroundColor = "yellow";
+   
+  } else {
+    ligne[i].style.backgroundColor = "lightcoral";
+    
   }
-
-
+  let message = "";//creating a new variable named message and initializing it as an empty string
+    if (abs < 3 && participation >= 3) {
+      message = "Good attendance – Excellent participation";
+    } else if (abs >= 3 && abs <= 4) {
+      message = "Warning – attendance low – You need to participate more";
+    } else if (abs >= 5) {
+      message = "Excluded – too many absences – You need to participate more";
+    } else {
+      message = "Keep going – improve participation";
+    }
+    ligne[i].cells[16].textContent = message;
+}
 };
